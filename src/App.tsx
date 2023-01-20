1389b7a697266b12acadecab16624c00a542b3e4
Geovane Borba <geovanne@live.com>
2023-01-20 19:39:06 -0300

    feat: add Button component, global styles and default theme styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyles } from './styles/global'
+
 function App() {
-  return <div className="App">Ignite Timer</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div className="App">
+        <Button />
+        <Button variant="secondary" />
+        <Button variant="danger" />
+        <Button variant="success" />
+      </div>
+
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
