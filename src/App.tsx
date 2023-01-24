87cd1de55e3096910f50f7140f8a8c64cf3242b4
Geovane Borba <geovanne@live.com>
2023-01-24 19:36:09 -0300

    feat: add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="App">
-        <Button />
-        <Button variant="secondary" />
-        <Button variant="danger" />
-        <Button variant="success" />
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyles />
     </ThemeProvider>
